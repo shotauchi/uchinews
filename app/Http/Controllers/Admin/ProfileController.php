0987c46b8647320caa24dc47d087004f045b323a
[shotauchi] <[tianneixiang84@gmail.com]>
2024-09-24 16:16:33 +0000

    コミット13

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 use App\Models\Profile;
 
+use App\Models\History2;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -88,6 +92,12 @@ class ProfileController extends Controller
 
         // 該当するデータを上書きして保存する
         $profile->fill($profile_form)->save();
+        
+        // 以下を追記
+        $history2 = new History2();
+        $history2->profile_id = $profile->id;
+        $history2->edited_at = Carbon::now();
+        $history2->save();
 
         return redirect('admin/profile');
     }
--- app/Models/History2.php
+++ app/Models/History2.php
@@ -5,20 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class History2 extends Model
 {
     use HasFactory;
     
     protected $guarded = array('id');
 
     public static $rules = array(
-        'title' => 'required',
-        'body' => 'required',
+        'profile_id' => 'required',
+        'edited_at' => 'required',
     );
-    // Profile Modelに関連付けを行う
-    public function histories()
-    {
-        return $this->hasMany('App\Models\History2');
-    }
 }
-
